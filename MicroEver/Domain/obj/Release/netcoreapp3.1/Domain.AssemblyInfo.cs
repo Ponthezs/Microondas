b06824a3695f0b446e32dd2dc76ff9d8caaa8463
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2024-10-15 19:44:59 -0300

    feat

--- MicroEver/Domain/obj/Release/netcoreapp3.1/Domain.AssemblyInfo.cs
+++ MicroEver/Domain/obj/Release/netcoreapp3.1/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dcfa5c714c7f151279dac3bcda15ebbdc9c4d92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+668c9b240ad4599a0f515126d5d078543c930490")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
